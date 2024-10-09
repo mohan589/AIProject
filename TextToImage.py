3c07c3e63bf739b4d5e6320540801baacaa7d93e
Mohan Pichikala <mpichikala@intralinks.com>
2024-10-09 11:31:42 +0530

    Updated Packages

--- TextToImage.py
+++ TextToImage.py
@@ -1,15 +1,15 @@
 from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 import torch
 
-model_id = "stabilityai/stable-diffusion-2"
-scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
-pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
+MODEL_ID = "stabilityai/stable-diffusion-2"
+scheduler = EulerDiscreteScheduler.from_pretrained(MODEL_ID, subfolder="scheduler")
+pipe = StableDiffusionPipeline.from_pretrained(MODEL_ID, scheduler=scheduler, torch_dtype=torch.float16)
 pipe = pipe.to("mps")
 
-prompt = "a photo of an astronaut riding a horse on mars"
-image = pipe(prompt).images[0]
+PROMPT = "a photo of an astronaut riding a horse on mars"
+image = pipe(PROMPT).images[0]
 image.save('data/astronaut_riding_horse_on_mars.png')
 
-prompt = "a photo of an iron man as saint"
-image = pipe(prompt).images[0]
+PROMPT = "a photo of an iron man as saint"
+image = pipe(PROMPT).images[0]
 image.save('data/iron_man_as_saint.png')
